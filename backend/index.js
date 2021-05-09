7a245a0eee2596274b568b189f7e2a6fa3ff8104
cthiesmann <christopher-thiesmann@web.de>
2021-05-09 23:00:46 +0200

    Fix express routing

--- backend/index.js
+++ backend/index.js
@@ -79,6 +79,11 @@ io.on('connection', (socket) => {
 
 app.use(express.static(path.join(__dirname, '../build')))
 
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 http.listen(port, () => {
 	console.log(`listening on *:${port}`)
 })
